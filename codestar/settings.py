04f0172fbcfa7b1bc62d523cfd91db90e70c6fe9
Monica Feis <Monica.Feis@eifer.org>
2026-07-08 10:35:12 +0200

    Added a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6q)l($0s&72dy-shu95r&p#*j4ye7lz1xhy$^409iut*(lihs%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -95,6 +95,11 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+    CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
