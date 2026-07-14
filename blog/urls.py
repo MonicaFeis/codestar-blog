15dc4cb404139c127e6deb310f610e5db99dac01
Monica Feis <Monica.Feis@eifer.org>
2026-07-14 09:49:32 +0200

    Added URL for the blog view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from . import views
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("blog/", include("blog.urls"), name="blog-urls"),
+    path('admin/', admin.site.urls),
 ]
